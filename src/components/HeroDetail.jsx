80a412407be35030f0dbcf94697d60103e59350e
AlexandraCimpoia <alexandra.cimpoia13@gmail.com>
2023-07-31 16:02:44 +0100

    added QL 3

--- src/components/HeroDetail.jsx
+++ src/components/HeroDetail.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import batman from '../images/batman.jpeg';
+import images from '../index';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-const HeroDetail = () => {
+const HeroDetail = (props) => {
+
+    <img src={images[props.info.alias.replaceAll(/\s/g, '')]} alt={props.info.name} height="200px" />
+
     return (
-        <div>
-            <img src={batman} alt="Batman" height="250px" />
-            <p id="blue-alias">Alias: Batman</p>
-            <p class="green-class">Name: Bruce Wayne</p>
-            <button class="green-class">Like</button>
-        </div>
+        <Card >
+            <div className="imgContainer">
+                <Card.Img variant="top" src={images[props.info.alias.replaceAll(/\s/g, '')]} width="150px" />
+            </div>
+            <Card.Body >
+                <Card.Title >Alias: {props.info.alias}</Card.Title>
+                <Card.Text >Name: {props.info.name}</Card.Text>
+                <Button variant="primary">Like</Button>
+            </Card.Body>
+        </Card>
     )
 }
 
